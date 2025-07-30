ec6108eeafd9c77fe25924c30a21dcff3f2f3d35
Shariar Akash <akashrssky@gmail.com>
2025-07-30 23:40:59 +0600

    Add clear button to search input
    Introduced a clear (X) button to the search input field in the SearchAndFilter component, allowing users to quickly clear their search query. Refactored imports for consistency and removed unused React import.

--- components/SearchAndFilter/index.tsx
+++ components/SearchAndFilter/index.tsx
@@ -1,8 +1,6 @@
 "use client";
-import React from "react";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useAtom } from "jotai";
-import { searchAtom, filterGenderAtom } from "@/store/search";
 import {
   Select,
   SelectContent,
@@ -10,18 +8,30 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { filterGenderAtom, searchAtom } from "@/store/search";
+import { useAtom } from "jotai";
+import { X } from "lucide-react";
 
 const SearchAndFilter = () => {
   const [search, setSearch] = useAtom(searchAtom);
   const [filterGender, setFilterGender] = useAtom(filterGenderAtom);
 
+  const handleClearSearch = () => {
+    setSearch("");
+  };
+
   return (
     <div className="flex gap-4">
-      <Input
-        placeholder="Search Products"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <div className="w-full relative">
+        <Input
+          placeholder="Search Products"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Button variant={"ghost"} size={"icon"} className="rounded-full absolute top-1/2 right-2 -translate-y-1/2" onClick={handleClearSearch}>
+          <X className="h-4 w-4" />
+        </Button>
+      </div>
       <Select
         value={filterGender}
         onValueChange={(value) => setFilterGender(value)}
